e9afc31fe66cc03ca0e476f1e78e4a0f4c398815
Benjamin Sherman <benjaminrsherman@gmail.com>
2017-07-16 23:33:17 -0400

    Fixed typo

--- Ajit-Pai_Bot.py
+++ Ajit-Pai_Bot.py
@@ -105,7 +105,7 @@ def parseText(comment, body, post):
 	try:
 
 		# Comment is related to net neutrality
-		if any(keyString in str.lower(body) for keytString in netNeutralityKeyStrings):
+		if any(keyString in str.lower(body) for keyString in netNeutralityKeyStrings):
 
 			# Pro net neutrality scan
 			proNN = 0
