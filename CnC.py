d569efeb358196d9bd7726e57e8b07c355b7f036
DailyDoseOfAwesome <keilmkelly@gmail.com>
2017-07-22 10:41:47 -0500

    cmds

--- CnC.py
+++ CnC.py
@@ -12,6 +12,7 @@ class CnC:
 		self.c2 = configFile['admins']['c2']
 		self.admins = configFile['admins']['admin'].split(",")
 		self.botname = configFile['admins']['botname']
+		self.subList = configFile['bot1']['subreddits']
 
 		return
 
@@ -22,15 +23,65 @@ class CnC:
 		commands = commands.splitlines()
 		restart = False
 		for command in commands:
-			if str.lower("all") in command or self.botname in command:
-				if str.lower("update") in command:
+			command = command.split(" ")
+			if str.lower("all") in command[0] or self.botname in command[0]:
+				if str.lower("update") in command[1]:
 					g = git.cmd.Git()
 					g.pull()
 					submission.reply(self.botname + ": Affirm, updating")
 					restart = True
 
-				if str.lower("ping") in command:
-					submission.reply(self.botname + ": Pong")
+				if str.lower("edit") in command[1]:
+					configFile = configparser.ConfigParser()
+					configFile.read('praw.ini')
+					try:
+						configFile.set(command[2], command[3], command[4])
+						with open('praw.ini', 'wb') as cfg:
+							cfg.write(configFile)
+							submission.reply(self.botname + ": Affirm editing " + command[2])
+						restart = True
+					except Exception as e:
+						submission.reply(self.botname + ": Instructions unclear, dick stuck in ceiling fan")
+						print(e)
+
+				if str.lower("add") in command[1]:
+					configFile = configparser.ConfigParser()
+					configFile.read('praw.ini')
+					existingVar = configFile[command[2]][command[3]]
+					try:
+						configFile.set(command[2], command[3], existingVar + "," + command[4])
+						with open('praw.ini', 'wb') as cfg:
+							cfg.write(configFile)
+							submission.reply(self.botname + ": Affirm, adding " + command[4])
+						restart = True
+					except Exception as e:
+						submission.reply(self.botname + ": Instructions unclear, dick stuck in ceiling fan")
+						print(e)
+
+				if str.lower("remove") in command[1]:
+					configFile = configparser.ConfigParser()
+					configFile.read('praw.ini')
+					existingVar = configFile[command[2]][command[3]]
+					if ("," + command[3]) in existingVar:
+						existingVar = "," + existingVar
+					elif (command[3] + ",") in existingVar:
+						existingVar = existingVar + ","
+					existingVar = existingVar.replace(command[3], "")
+					try:
+						configFile.set(command[2], command[3], existingVar)
+						with open('praw.ini', 'wb') as cfg:
+							cfg.write(configFile)
+							submission.reply(self.botname + ": Affirm, removing " + command[4])
+						restart = True
+					except Exception as e:
+						submission.reply(self.botname + ": Instructions unclear, dick stuck in ceiling fan")
+						print(e)
+
+				if str.lower("status") in command[1]:
+					submission.reply(self.botname + ": Alive\n\n" + self.subList)
+
+				if str.lower("die") in command[1]:
+					exit()
 
 				with open('cncLog.txt', 'w') as log:
 					log.write(str(submission.id))
